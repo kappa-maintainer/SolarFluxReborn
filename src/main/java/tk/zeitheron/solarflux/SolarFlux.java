89930887f92f57d85152883ad7f0080b711ed212
kappa-maintainer <11038263+kappa-maintainer@users.noreply.github.com>
2023-12-24 20:28:56 +0800

    Update SolarFlux.java

--- src/main/java/tk/zeitheron/solarflux/SolarFlux.java
+++ src/main/java/tk/zeitheron/solarflux/SolarFlux.java
@@ -52,7 +52,7 @@ public class SolarFlux
 {
 	public static final Logger LOG = LogManager.getLogger("SolarFlux");
 
-	public static final File CONFIG_DIR = null;
+	public static File CONFIG_DIR = null;
 
 	@SidedProxy(clientSide = InfoSF.PROXY_CLIENT, serverSide = InfoSF.PROXY_SERVER)
 	public static ISFProxy proxy;
@@ -125,7 +125,7 @@ public class SolarFlux
 
 		File cfgDir = new File(e.getModConfigurationDirectory(), InfoSF.MOD_ID);
 		if(!cfgDir.isDirectory()) cfgDir.mkdirs();
-		FinalFieldHelper.setStaticFinalField(SolarFlux.class, "CONFIG_DIR", cfgDir);
+		SolarFlux.CONFIG_DIR = cfgDir;
 
 		{
 			File modCfgFile = new File(cfgDir, "compats.hlc");
@@ -211,7 +211,7 @@ public class SolarFlux
 	@EventHandler
 	public void init(FMLInitializationEvent e)
 	{
-		FinalFieldHelper.setStaticFinalField(NetworkSF.class, "INSTANCE", new NetworkSF());
+		NetworkSF.INSTANCE = new NetworkSF();
 
 		NetworkRegistry.INSTANCE.registerGuiHandler(instance, new GuiHandlerSF());
 		proxy.init();
@@ -253,54 +253,5 @@ public class SolarFlux
 		e.registerServerCommand(new CommandSolarFlux());
 	}
 
-	public static class FinalFieldHelper
-	{
-		private static Field modifiersField;
-		private static Object reflectionFactory;
-		private static Method newFieldAccessor;
-		private static Method fieldAccessorSet;
 
-		static boolean setStaticFinalField(Class<?> cls, String var, Object val)
-		{
-			try
-			{
-				Field f = cls.getDeclaredField(var);
-				if(Modifier.isStatic(f.getModifiers()))
-					return setFinalField(f, null, val);
-				return false;
-			} catch(Throwable err)
-			{
-				err.printStackTrace();
-			}
-			return false;
-		}
-
-		static Field makeWritable(Field f) throws ReflectiveOperationException
-		{
-			f.setAccessible(true);
-			if(modifiersField == null)
-			{
-				Method getReflectionFactory = Class.forName("sun.reflect.ReflectionFactory").getDeclaredMethod("getReflectionFactory");
-				reflectionFactory = getReflectionFactory.invoke(null);
-				newFieldAccessor = Class.forName("sun.reflect.ReflectionFactory").getDeclaredMethod("newFieldAccessor", Field.class, boolean.class);
-				fieldAccessorSet = Class.forName("sun.reflect.FieldAccessor").getDeclaredMethod("set", Object.class, Object.class);
-				modifiersField = Field.class.getDeclaredField("modifiers");
-				modifiersField.setAccessible(true);
-			}
-			modifiersField.setInt(f, f.getModifiers() & ~Modifier.FINAL);
-			return f;
-		}
-
-		public static boolean setFinalField(Field f, @Nullable Object instance, Object thing) throws ReflectiveOperationException
-		{
-			if(Modifier.isFinal(f.getModifiers()))
-			{
-				makeWritable(f);
-				Object fieldAccessor = newFieldAccessor.invoke(reflectionFactory, f, false);
-				fieldAccessorSet.invoke(fieldAccessor, instance, thing);
-				return true;
-			}
-			return false;
-		}
-	}
 }
